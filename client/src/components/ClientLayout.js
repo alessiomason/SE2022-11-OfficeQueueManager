1ae5ad8667994b3f28a37fcc414dcd6b6d2640e9
luca-rota <lucarota981@gmail.com>
2022-10-17 18:59:27 +0200

    .

--- client/src/components/ClientLayout.js
+++ client/src/components/ClientLayout.js
@@ -1,7 +1,80 @@
-function MyClientLayout() { 
-    return ( 
-        <></>
-    );
+import { Button, Col, Form, Nav, Row, ListGroup, Navbar, Container, NavDropdown, ButtonGroup } from "react-bootstrap";
+import '../App.css';
+
+import { default as Confirm } from '../icons/confirm.svg';
+
+function MyClientLayout() {
+  return ( 
+        <>
+      <Container>
+        <Row>
+          <Col md={{ span: 5, offset: 1 }}>
+            <Form >
+              <Row className="client-border1">
+                <Col md={{ span: 8, offset: 1 }}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter client name" />
+                  </Form.Group>
+                </Col>
+                <Col md={3}>
+                  <Button variant="primary" type="submit" > Submit </Button>
+                </Col>
+              </Row>
+            </Form>
+            <Row className="client-border2 mt-5">
+              <Col md={{ span: 5, offset: 1 }}>
+                <Col md={{ span: 8, offset: 4 }}>
+                  <h4 > Your tickets: </h4>
+                </Col>
+                <ListGroup variant="flush">
+                  <ListGroup.Item action className='ticket-item'>
+                    aaaaaaaaaaaaaaaaaaaaaaaa
+                  </ListGroup.Item>
+                  <ListGroup.Item action className='ticket-item' >
+                    bbbbbbbbbbbbbbbbbbbbbbbb
+                  </ListGroup.Item>
+                  <ListGroup.Item action className='ticket-item'>
+                    cccccccccccccccccccccccc
+                  </ListGroup.Item>
+                  <ListGroup.Item action className='ticket-item'>
+                    dddddddddddddddddddddddd
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+            </Row>
+          </Col>
+          <Col md={{span: 4, offset: 1 }} className="client-border3">
+                <Col md={{ span: 8, offset: 4 }}>
+                  <h4 > Services list: </h4>
+                </Col>
+                <ListGroup variant="flush">
+                  <ListGroup.Item action className='ticket-item'>
+                    aaaaaaaaaaaaaaaaaaaaaaaa
+                    <Button> <img src={Confirm} alt="confirm" /></Button>
+                  </ListGroup.Item>
+                  <ListGroup.Item action className='ticket-item' >
+                    bbbbbbbbbbbbbbbbbbbbbbbb
+                    <Button> <img src={Confirm} alt="confirm" /></Button>
+                  </ListGroup.Item>
+                  <ListGroup.Item action className='ticket-item'>
+                    cccccccccccccccccccccccc
+                    <Button> <img src={Confirm} alt="confirm" /></Button>
+                  </ListGroup.Item>
+                  <ListGroup.Item action className='ticket-item'>
+                    dddddddddddddddddddddddd
+                    <Button> <img src={Confirm} alt="confirm" /></Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+            </Row>
+            <Row className="mt-5">
+              <Col md={{span:6, offset: 5}} >
+                <Button> Show the pannel to check queues</Button>
+              </Col>
+            </Row>
+          </Container>
+        </>
+        );
 }
 
-export default MyClientLayout;
+        export default MyClientLayout;
